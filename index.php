3c2c14463f835d21b40b0542526c98b3c63dd819
cassiano-souza-jdk <cassianomaximiano959@gmail.com>
2025-06-05 15:06:35 -0300

    first part index

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>Systask</title>
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="justify-content-center">
+            <div class="col-12 col-sm-8 col-md-6">
+                <form action="" method="post">
+                    <div class="form-group row">
+                        <div class="col-12 col-sm-8 col-md-4">
+                            <label>Data</label>
+                            <input type="date" name="data" id="data">
+                        </div>
+
+                        <div class="col-12 col-sm-8 col-md-8">
+                            <label>Tarefa</label>
+                            <input type="text" name="tarefa" id="tarefa">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <div class="col-12 col-sm-12 col-md-12">
+                            <label>Descrição</label>
+                            <input type="text" name="descricao" id="descricao">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <div class="justify-content-end col-12 col-sm-8 col-md-6">
+                        <button type="submit" class="btn btn-outline-primary btn-lg btn-block">Registrar</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
